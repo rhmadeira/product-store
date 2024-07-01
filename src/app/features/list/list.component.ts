3898e328508bc88c9600340b71ececd383456120
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-06-30 21:18:43 -0300

    feat: add card material

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,16 +1,18 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { IProduct } from '../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
 export class ListComponent {
-  products: any[] = [];
+  products: IProduct[] = [];
   ProductsService = inject(ProductsService);
 
   ngOnInit() {
