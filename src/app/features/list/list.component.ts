074795a12043c3c0fe945aaae7732de724d1dbeb
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-06-30 21:05:14 -0300

    refactor: product listing

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
   imports: [],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
 export class ListComponent {
+  products: any[] = [];
+  ProductsService = inject(ProductsService);
 
+  ngOnInit() {
+    this.ProductsService.getAll().subscribe((products) => {
+      this.products = products;
+    });
+  }
 }
