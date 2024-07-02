dbae5137324cedbbb3477d9e12b2ffce72566efa
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-07-01 22:26:25 -0300

    refactor: refactor product listing , creating component

---
+++ src/app/features/list/components/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, computed, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { IProduct } from '../../../../shared/interfaces/product.interface';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {
+  product = input.required<IProduct>();
+  productTitle = computed(() => this.product().title);
+}
--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,13 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 import { IProduct } from '../../shared/interfaces/product.interface';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
